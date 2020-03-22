4c04c7f3bf2cc86a6bcc4e412dfda5d999371e20
Oscarchoi <wychoi502@gmail.com>
2020-03-22 18:55:22 +0900

    Updates

---
+++ precache-manifest.5ec95259c71bfb9665b8754b61ac40fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1564465d9ecb84302f28b0c75fc08f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90809b39b762b58406ba",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "ed907f169c10c3599395",
+    "url": "/static/js/2.1313a866.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1313a866.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90809b39b762b58406ba",
+    "url": "/static/js/main.6f7bd9a0.chunk.js"
+  },
+  {
+    "revision": "f348e065194ab26c7668",
+    "url": "/static/js/runtime-main.14ccbc57.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
