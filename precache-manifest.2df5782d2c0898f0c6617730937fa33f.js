665ee1e4a2cd9401e88ea84e134705a7d79df51d
Oscarchoi <wychoi502@gmail.com>
2020-05-27 00:52:28 +0900

    Updates

---
+++ precache-manifest.2df5782d2c0898f0c6617730937fa33f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bbb98993a03e8bdbc552a8fd7cec632",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c530c45e284eb7fb2ad0",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "943e1d75673f0a12874a",
+    "url": "/static/js/2.8011eb52.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.8011eb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c530c45e284eb7fb2ad0",
+    "url": "/static/js/main.cc92b082.chunk.js"
+  },
+  {
+    "revision": "b4ac422dcc6b461fed3e",
+    "url": "/static/js/runtime-main.3c810ab2.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
