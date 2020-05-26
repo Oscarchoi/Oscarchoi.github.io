0725b0294aeb3f3c139dde8ee1d871c660815ed0
Oscarchoi <wychoi502@gmail.com>
2020-05-27 00:30:59 +0900

    Add material-ui core & AppBar Layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+import SearchAppBar from './SearchAppBar';
+
 import logo from './logo.svg';
 import './App.css';
 
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
+
+
 function App() {
+  const stlyes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <SearchAppBar title="Oscarchoi's Blogs" />
+      <main>
+        <div className={stlyes.heroContent}>
+          <Container maxWidth="sm" backgroundColor="grey">
+            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+              Oscarchoi's Blogs
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+              Something short and leading about the collection below—its contents, the creator, etc.
+              Make it short and sweet, but not too short so folks don&apos;t simply skip over it
+              entirely.
+            </Typography>
+          </Container>
+        </div>
+      </main>
+    </React.Fragment>
   );
 }
+
+
 
 export default App;
